1b66ee02e0b635513fdbf96d04e449dcd2cc2363
NotNite <hi@notnite.com>
2023-05-02 10:17:50 -0400

    client is nixed

--- flake.nix
+++ flake.nix
@@ -27,13 +27,30 @@
             nativeBuildInputs = with pkgs; [ openssl ];
           };
 
+          packages.loremipsum-client = craneLib.buildPackage {
+            pname = "client";
+            version = "0.1.0";
+            src = src;
+            extraCargoFlags = "-p client";
+            doCheck = false;
+
+            buildInputs = with pkgs; [ pkg-config ];
+            nativeBuildInputs = with pkgs; [ openssl ];
+          };
+
           apps.loremipsum-server = flake-utils.lib.mkApp {
             name = "loremipsum-server";
             drv = packages.loremipsum-server;
           };
 
+          apsp.loremipsum-client = flake-utils.lib.mkApp {
+            name = "loremipsum-client";
+            drv = packages.loremipsum-client;
+          };
+
           devShell = pkgs.mkShell {
-            inputsFrom = [ packages.loremipsum-server ];
+            inputsFrom =
+              [ packages.loremipsum-server packages.loremipsum-client ];
             nativeBuildInputs = with pkgs; [
               rustc
               cargo
@@ -43,86 +60,126 @@
           };
         });
     in packages // {
-      nixosModule = { config, lib, pkgs, ... }:
-        with lib;
-        let
-          cfgServer = config.services.loremipsum-server;
-          pkgServer = self.packages.${pkgs.system}.loremipsum-server;
-          #cfgClient = config.services.loremipsum-client;
-          #pkgClient = self.packages.${pkgs.system}.loremipsum-client;
-        in {
-          options.services.loremipsum-server = {
-            enable = mkEnableOption description;
+      nixosModules = {
+        loremipsum-server = { config, lib, pkgs, ... }:
+          with lib;
+          let
+            cfgServer = config.services.loremipsum-server;
+            pkgServer = self.packages.${pkgs.system}.loremipsum-server;
+          in {
+            options.services.loremipsum-server = {
+              enable = mkEnableOption description;
 
-            port = mkOption {
-              type = types.int;
-              default = 56552;
-            };
-
-            schedules = mkOption {
-              type = types.attrsOf types.str;
-              default = { };
-            };
-          };
-
-          config = mkIf cfgServer.enable {
-            users = {
-              users.loremipsum = {
-                isSystemUser = true;
-                group = "loremipsum";
+              port = mkOption {
+                type = types.int;
+                default = 56552;
               };
 
-              groups.loremipsum = { };
-            };
-
-            systemd.services.loremipsum-server = let
-              cfgFile = pkgs.writeText "config.toml" ''
-                schedules = { ${
-                  concatStringsSep ", "
-                  (mapAttrsToList (name: schedule: ''${name} = "${schedule}"'')
-                    cfgServer.schedules)
-                } }
-
-                [database]
-                host = "localhost"
-                port = 5432
-                username = "loremipsum"
-                password = "loremipsum"
-                database = "loremipsum"
-
-                [api]
-                address = "0.0.0.0:${toString cfgServer.port}"
-              '';
-            in {
-              description = "loremipsum server";
-              after = [ "network.target" ];
-              wantedBy = [ "multi-user.target" ];
-
-              serviceConfig = {
-                User = "loremipsum";
-                Group = "loremipsum";
-                ExecStart = "${pkgServer}/bin/server ${cfgFile}";
+              schedules = mkOption {
+                type = types.attrsOf types.str;
+                default = { };
               };
             };
 
-            services.postgresql = {
-              enable = mkDefault true;
-              authentication = ''
-                local loremipsum loremipsum trust
-                host loremipsum loremipsum localhost trust
-              '';
-              ensureDatabases = [ "loremipsum" ];
+            config = mkIf cfgServer.enable {
+              users = {
+                users.loremipsum = {
+                  isSystemUser = true;
+                  group = "loremipsum";
+                };
 
-              ensureUsers = [{
-                name = "loremipsum";
-                ensurePermissions."DATABASE \"loremipsum\"" = "ALL PRIVILEGES";
-              }];
+                groups.loremipsum = { };
+              };
 
-              package = lib.mkForce pkgs.postgresql_13;
-              extraPlugins = with pkgs.postgresql_13.pkgs; [ timescaledb ];
-              settings.shared_preload_libraries = "timescaledb";
+              systemd.services.loremipsum-server = let
+                cfgFile = pkgs.writeText "config.toml" ''
+                  schedules = { ${
+                    concatStringsSep ", " (mapAttrsToList
+                      (name: schedule: ''${name} = "${schedule}"'')
+                      cfgServer.schedules)
+                  } }
+
+                  [database]
+                  host = "localhost"
+                  port = 5432
+                  username = "loremipsum"
+                  password = "loremipsum"
+                  database = "loremipsum"
+
+                  [api]
+                  address = "0.0.0.0:${toString cfgServer.port}"
+                '';
+              in {
+                description = "loremipsum server";
+                after = [ "network.target" ];
+                wantedBy = [ "multi-user.target" ];
+
+                serviceConfig = {
+                  User = "loremipsum";
+                  Group = "loremipsum";
+                  ExecStart = "${pkgServer}/bin/server ${cfgFile}";
+                };
+              };
+
+              services.postgresql = {
+                enable = mkDefault true;
+                authentication = ''
+                  local loremipsum loremipsum trust
+                  host loremipsum loremipsum localhost trust
+                '';
+                ensureDatabases = [ "loremipsum" ];
+
+                ensureUsers = [{
+                  name = "loremipsum";
+                  ensurePermissions."DATABASE \"loremipsum\"" =
+                    "ALL PRIVILEGES";
+                }];
+
+                package = lib.mkForce pkgs.postgresql_13;
+                extraPlugins = with pkgs.postgresql_13.pkgs; [ timescaledb ];
+                settings.shared_preload_libraries = "timescaledb";
+              };
             };
           };
-        };
+
+        loremipsum-client = { config, lib, pkgs, ... }:
+          with lib;
+          let
+            cfgClient = config.services.loremipsum-client;
+            pkgClient = self.packages.${pkgs.system}.loremipsum-client;
+          in {
+            options.services.loremipsum-client = {
+              enable = mkEnableOption description;
+
+              configFile = mkOption {
+                type = types.str;
+                default = "/etc/loremipsum-client.toml";
+              };
+            };
+
+            config = mkIf cfgClient.enable {
+              users = {
+                users.loremipsum = {
+                  isSystemUser = true;
+                  group = "loremipsum";
+                };
+
+                groups.loremipsum = { };
+              };
+
+              systemd.services.loremipsum-client = {
+                description = "loremipsum client";
+                after = [ "network.target" ];
+                wantedBy = [ "multi-user.target" ];
+
+                serviceConfig = {
+                  User = "loremipsum";
+                  Group = "loremipsum";
+                  ExecStart = "${pkgClient}/bin/client ${cfgClient.configFile}";
+                };
+              };
+            };
+          };
+      };
     };
 }
